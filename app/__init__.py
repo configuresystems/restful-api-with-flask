b8fc2efa168e28e4a87a8f71285a1747ec62bfc8
john martin <j.martin@configure.systems>
2014-12-13 02:48:20 +0000

    pep8 compliance

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify
 
+
 # instantiate Flask so that we may use it!
 app = Flask(__name__)
 
 # this is our index route, found here
 # DEFAULT: http://[hostname]:5000/hello-world
+
+
 @app.route('/hello-world')
 def index():
-    return jsonify( {"message": "Hello World!" } )
-
+    return jsonify({"message": "Hello World!"})
--- app/testing.py
+++ app/testing.py
@@ -4,28 +4,21 @@ from app import app
 
 
 class HelloWorldTest(TestCase):
-    """
-    Our first unittest to check the response code, payload type, and payload
+    """Our first unittest to check the response code, payload type, and payload
     """
     def create_app(self):
-        """
-        Here we can set application configurations so flag our application
-        as testing, in case we build verbose functions or something
-        """
+        """Here we can set application configurations so flag our application
+        as testing, in case we build verbose functions or something"""
         app.config['TESTING'] = True
         return app
 
     def test_hello_world(self):
-        """
-        We test three things against the index route
+        """We test three things against the index route
         reponse code == 200
         response.message = "Hello World!"
-        response payload is JSON
-        """
+        response payload is JSON"""
         with self.client:
-            response = self.client.get(
-                    url_for('index')
-                    )
+            response = self.client.get(url_for('index'))
             # is it a 200 response
             self.assert200(response)
             # is the payload a JSON document
